082e89820500c3f1eea7619c8f79de100336f6a8
Theodore G <95758907+theodoregi@users.noreply.github.com>
2023-12-05 10:19:56 +0100

    Update ml_main.py

--- ML/ml_main.py
+++ ML/ml_main.py
@@ -12,6 +12,7 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
 from tensorflow.keras.layers import InputLayer
 import tensorflow.keras.layers as layers
 from tensorflow.keras import backend as K
+from tensorflow.keras.utils import normalize
 from tensorflow.keras.layers import Dense, Reshape
 
 
@@ -20,7 +21,7 @@ def create_set(path):
     validation_set = []
     for filename in os.listdir(path):
         if filename.endswith('.png'):
-            if random.random() < 0.7:
+            if random.random() < 0.5:
                 train_set.append(filename)
             else:
                 validation_set.append(filename)
@@ -51,23 +52,21 @@ y_train = [Image.open(filename) for filename in y_train_files]
 x_train = np.array(x_train)
 y_train = np.array(y_train)
 
+x_train = normalize(x_train, axis=1)
+y_train = normalize(y_train, axis=1)
+
 model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=(img_rows, img_cols, 3)))
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(img_rows, img_cols, 3)))
+model.add(MaxPooling2D(pool_size=(10, 10)))
 model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(MaxPooling2D(pool_size=(10, 10)))
 model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-last = Dense(np.prod(output_shape), activation='sigmoid')
-reshaped_output = Reshape(output_shape)
-model.add(last)
-model.add(reshaped_output)
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(np.prod(output_shape), activation='sigmoid'))
+model.add(Reshape(output_shape))
 
 model.summary()
-
 
 model.compile(loss=tensorflow.keras.losses.binary_crossentropy,
                 optimizer=tensorflow.keras.optimizers.Adadelta(),
@@ -87,6 +86,12 @@ for filename in validation_set:
 
 x_test = [Image.open(filename) for filename in x_test_files]
 y_test = [Image.open(filename) for filename in y_test_files]
+
+x_test = np.array(x_test)
+y_test = np.array(y_test)
+
+x_test = normalize(x_test, axis=1)
+y_test = normalize(y_test, axis=1)
 
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
